7bf52895b21e9840245b979037d5791b75313e1b
victor <victorugwueze@gmail.com>
2018-10-02 20:52:45 +0100

    bug(travis-error): fix travis build error
    - add .eslintignore file
       add covergae and .nyc_output to .eslintignore file
    - update migrations directory path inside .sequelizerc file
    - update config path on server/models/index.js file
    
    [Fixes #160924061]

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
+const config = require('../../config/config')[env];
 
 const basename = path.basename(__filename);
 
