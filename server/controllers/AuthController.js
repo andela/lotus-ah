1b63b4bcac0a1efbd6b98a88575e4e137c961ca4
victor <victorugwueze@gmail.com>
2018-10-05 17:20:55 +0100

    feat(reset passwod): reset password via email
     - add AuthController
       AuthController has two methods
        * resetPassword
        * forgotPassword
    
    - add a validator to midleware/validator.js
      The validator checks the validity
      of the token
    
    [Finishes #160451597]

---
+++ server/controllers/AuthController.js
@@ -0,0 +1,80 @@
+// Third party libraries
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+// User model
+import { User } from '../db/models';
+
+import EmailController from './EmailController';
+
+
+/**
+ *
+ *
+ * @class AuthController
+ * @description for user password authentication
+ */
+class AuthController {
+  /**
+   * @static
+   * @param {object} request
+   * @param {object} response
+   * @description verifies that user exists
+   * @return {object} response
+   * @memberof AuthController
+   */
+  static forgotPassword(request, response) {
+    User.findOne({
+      where: { email: `${request.body.email}` },
+    })
+      .then((result) => {
+        if (result === null) {
+          return response.status(404)
+            .json({
+              status: 'success',
+              message: 'No user with email found',
+            });
+        }
+        // Sing token
+        const token = jwt.sign({ userId: result.id }, process.env.JWT_SECRET, {
+          expiresIn: '1hr',
+        });
+        const link = `/api/v1/auth/reset_password?token=${token}`;
+        // Send mail
+        EmailController.validationEmail(result);
+        return response.status(200)
+          .json({
+            status: 'success',
+            message: 'Password reset details has been sent to your mail',
+            link,
+          });
+      })
+      .catch(error => error);
+  }
+
+
+  /**
+   * @static
+   * @param {object} request
+   * @param {object} response
+   * @description resets user password
+   * @returns {object} body
+   * @memberof AuthController
+   */
+  static resetPassword(request, response) {
+    const hash = bcrypt.hashSync(request.body.newPassword, 10);
+    User.update(
+      { password: hash },
+      { where: { id: request.body.userId } },
+    ).then(() => response.status(200)
+      .json({
+        message: 'Password changed successfully',
+      }))
+      .catch(() => response.status(400)
+        .json({
+          status: 'Failed',
+          message: 'Problem changing password',
+        }));
+  }
+}
+export default AuthController;
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import users from './users';
+import auth from './auth';
+import createUser from '../../fixtures/user';
 
 const router = Router();
 router.use('/', users);
+router.use('/', auth);
+router.post('/users/create', createUser);
 
 export default router;
