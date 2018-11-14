e0f686cfb0f6c64faf5e4a4bfdd8a8b55f2a0521
andela-phemmy <femi.adegoke@andela.com>
2018-11-14 19:45:48 +0100

    fix failing travis test

--- tests/socialAuth.test.js
+++ tests/socialAuth.test.js
@@ -6,18 +6,6 @@ import { mockedUser } from '../server/helpers/mockStrategy';
 const { expect } = chai;
 chai.use(chaiHttp);
 
-describe('GET /auth/facebook', () => {
-  it('should return a user object when user successfully authenticates facebook', (done) => {
-    chai
-      .request(server)
-      .get('/api/v1/auth/facebook/redirect')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-});
-
 describe('GET /auth/google', () => {
   it('should return a user object when user successfully authenticates google', (done) => {
     mockedUser.emails = [{ value: 'joshua@gmail.com' }];
